41ce2e5d1d80a3d78a4ec188ab39105ca34788fa
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-04-14 13:12:59 +0200

    interface creation

--- dist/main.js
+++ dist/main.js
@@ -3,3 +3,16 @@ var getFullName = function (name, surname) {
     return name + " " + surname;
 };
 console.log(getFullName("mordi", "goldstein"));
+var user = {
+    name: "monster",
+    age: 30,
+    getMessage: function () {
+        return "Hello" + name;
+    }
+};
+var user2 = {
+    name: "Jack",
+    getMessage: function () {
+        return "Hello";
+    }
+};
--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,24 @@ const getFullName = (name: string, surname: string): string => {
 };
 
 console.log(getFullName("mordi", "goldstein"));
+
+interface User {
+  name: string;
+  age?: number;
+  getMessage(): string;
+}
+
+const user: User = {
+  name: "monster",
+  age: 30,
+  getMessage() {
+    return "Hello" + name;
+  },
+};
+
+const user2: User = {
+  name: "Jack",
+  getMessage(){
+      return "Hello"
+  }
+};
